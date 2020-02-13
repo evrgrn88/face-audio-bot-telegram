b880ed0118917c186e76fdd44d52c5a68b97af00
evrgrn88 <evrgrn88@gmail.com>
2020-02-13 17:20:04 +0300

    cleaned up a bit

--- source.py
+++ source.py
@@ -1,7 +1,6 @@
 import requests
 import os
 import re
-# import sys
 
 import telebot # pip install telebot
 import ffmpeg # pip install ffmpeg-python
@@ -19,11 +18,6 @@ bot = telebot.TeleBot(apiKey)
 imageNamePattern = "image_"
 audioNamePattern = "audio_message_"
 
-
-# @bot.message_handler(content_types=["text", "audio", "video", "document", "sticker"])
-# def start_message(message):
-#   bot.send_message(message.chat.id, "Хуй 8======э")
-
 def fileCount(pattern, extension):
   checkFiles = glob2.glob("*." + str(extension))
   max = 0
@@ -32,7 +26,6 @@ def fileCount(pattern, extension):
     num = 0
     if findFile is not None:
       num = int(findFile.group(1))
-    # num = int(re.search(str(pattern) + "(\d*)", name).group(1))
     if num > max:
       max = num
     else:
@@ -45,8 +38,6 @@ def checkFace(img, newName):
   faces = faceCascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=3, minSize=(30, 30))
   if len(faces) > 0:
     print("face recognized")
-    for (x, y, w, h) in faces:
-      cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
     os.rename(img, newName + str(fileCount(newName, "jpg")) + ".jpg")
   else:
     print("face not recognized")
@@ -66,53 +57,23 @@ def convertToWav(inFile, outFile):
     .overwrite_output()
     .run(capture_stdout=True)
   )
-
   os.remove(inFile)
 
 @bot.message_handler(content_types=["voice"])
 def start_message(message):
-  # bot.send_message(message.chat.id, "Хуй 8======э")
-  # file_info = bot.get_file(message.voice.file_id)
-  # file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(apiKey, file_info.file_path))
-
-  # with open(tempName, "wb") as f:
-  #   f.write(file.content)
-  
   tempName = "temp.ogg"
   voiceId = message.voice.file_id
   downloadFile(voiceId, tempName)
   convertToWav(tempName, audioNamePattern)
 
-  # (ffmpeg
-  #   .input(tempName)
-  #   .output(audioNamePattern + str(fileCount(audioNamePattern, "wav")) + ".wav", ar='16k')
-  #   .overwrite_output()
-  #   .run(capture_stdout=True)
-  # )
-
-  # os.remove(tempName)
-
 @bot.message_handler(content_types=["photo"])
 def start_message(message):
-  # file_info = bot.get_file(message.photo[-1].file_id)
-  # file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(apiKey, file_info.file_path))
-  
   tempName = "temp.jpg"
   imgId = message.photo[-1].file_id
   downloadFile(imgId, tempName)
   checkFace(tempName, imageNamePattern)
 
-  # with open(tempName, 'wb') as newFile:
-  #   newFile.write(bot.download_file(file_info.file_path))
 
-  
-
-      
-  # uid = message.from_user.id
-  # print(file_info)
-
-# @bot.message_handler(commands=['start'])
-# def start(message):
-#   print(message)
+# uid = message.from_user.id
 
 bot.polling()
